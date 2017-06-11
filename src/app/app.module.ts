2039a21c3ff48956fcc4e4ddb3ea6d880b2ac47e
Alex <alex.lomakovskiy@gmail.com>
2017-06-11 22:07:41 +0300

    Finished 'Tour of heroes'

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,12 @@
+import {AppRoutingModule} from './app-routing.module'
 import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {NgModule} from '@angular/core';
+
+// Imports for loading & configuring the in-memory web api
+import {InMemoryWebApiModule} from 'angular-in-memory-web-api';
+import {InMemoryDataService} from './in-memory-data.service';
 
 import {AppComponent} from './app.component';
 import {DashboardComponent} from './dashboard.component';
@@ -8,20 +14,23 @@ import {HeroDetailComponent} from './hero-detail.component';
 import {HeroesComponent} from './heroes.component';
 
 import {HeroService} from './hero.service';
+import { HeroSearchComponent } from './hero-search.component';
 
-import {AppRoutingModule} from './app-routing.module'
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
     HeroDetailComponent,
-    HeroesComponent
+    HeroesComponent,
+    HeroSearchComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
     FormsModule,
-    AppRoutingModule
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService)
   ],
   providers: [HeroService],
   bootstrap: [AppComponent]
